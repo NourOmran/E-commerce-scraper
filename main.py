193abb4842f6766c3893f9ebae61379ceb7c278f
nouromran <nourmustafaomran98@gmail.com>
2023-06-15 06:13:28 +0300

    Test 2

--- main.py
+++ main.py
@@ -6,13 +6,71 @@ import time
 import re
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import csv
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True )
+options.add_argument("headless")
 driver = webdriver.Chrome(options=options)
 
 def ulta():
 	driver.get("https://www.ulta.com/search?search=mango+butter+")
+	wait = WebDriverWait(driver, 10)
+	driver.implicitly_wait(20)
+	while True:
+		try:
+			wait.until(EC.element_to_be_clickable((By.XPATH, """//button[normalize-space()='Show More Products']    """))).click()
+			time.sleep(5)
+		except:
+			break
+	fetch = driver.find_element(By.XPATH, """/html/body/div[5]/div/div/div/div/main/div[5]/div/div/div/div/div[3]/ul""")
+	links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',fetch.get_attribute("innerHTML"))
+	product_links = []
+	for link in links :
+		if '/p/' in link:
+			product_links.append(link)
+	products =[]
+	for link in product_links:
+
+		driver.implicitly_wait(10)
+		driver.get(link)
+
+
+		webdriver.support.expected_conditions.visibility_of( """//summary[@id='Ingredients']//h3[@class='Accordion_Huge__summary__header']""")
+		try:
+
+
+			click_agian = driver.find_element(By.XPATH , """//summary[@id='Ingredients']//h3[@class='Accordion_Huge__summary__header']""")
+			click_agian.click()
+
+			ingredients = driver.find_element(By.XPATH,"""//details[@aria-controls='Ingredients']//div[@class='Accordion_Huge__content']//div[@class='Markdown Markdown--body-2']""")
+			#print(ingredients.text)
+			category = driver.find_element(By.XPATH , """//ul[@id='Breadcrumbs__List']""")
+			brand_name = driver.find_element(By.XPATH , """//span[@class='Text-ds Text-ds--title-5 Text-ds--left']""")
+			company = driver.find_element(By.XPATH , """//*[@id="92384e5c-2234-4e8f-bef7-e80391889cfc"]/h1/span[1]/a""")
+
+			err = True
+		except :
+			print(link)
+			err = False
+
+		if err:
+			product = [company.text,brand_name.text,category.text ,ingredients.text,link]
+			products.append(product)
+	fields = ['company', 'product name ', 'category' ,'ingredients', 'link']
+	with open('ulta.csv', 'w') as f:
+
+		# Create a CSV writer object that will write to the file 'f'
+		csv_writer = csv.writer(f)
+
+		# Write the field names (column headers) to the first row of the CSV file
+		csv_writer.writerow(fields)
+
+		# Write all of the rows of data to the CSV file
+		csv_writer.writerows(products)
+
+
+def sephora():
+	driver.get("https://www.sephora.com/search?keyword=hair%20oil")
 	wait = WebDriverWait(driver, 10)
 	driver.implicitly_wait(20)
 	while True:
@@ -27,39 +85,47 @@ def ulta():
 	for link in links :
 		if '/p/' in link:
 			product_links.append(link)
-
+	products =[]
 	for link in product_links:
-		print(link)
+
 		driver.implicitly_wait(10)
 		driver.get(link)
 
 
 		webdriver.support.expected_conditions.visibility_of( """//summary[@id='Ingredients']//h3[@class='Accordion_Huge__summary__header']""")
 		try:
-			ingredients = driver.find_element(By.XPATH,"""/html[1]/body[1]/div[5]/div[1]/div[1]/div[1]/div[1]/main[1]/div[3]/div[1]/div[9]/div[1]/div[1]/details[3]/div[1]/div[1]/p[1]""")
+
+
+			click_agian = driver.find_element(By.XPATH , """//summary[@id='Ingredients']//h3[@class='Accordion_Huge__summary__header']""")
+			click_agian.click()
+
+			ingredients = driver.find_element(By.XPATH,"""//details[@aria-controls='Ingredients']//div[@class='Accordion_Huge__content']//div[@class='Markdown Markdown--body-2']""")
+			#print(ingredients.text)
 			category = driver.find_element(By.XPATH , """//ul[@id='Breadcrumbs__List']""")
 			brand_name = driver.find_element(By.XPATH , """//span[@class='Text-ds Text-ds--title-5 Text-ds--left']""")
+			company = driver.find_element(By.XPATH , """//*[@id="92384e5c-2234-4e8f-bef7-e80391889cfc"]/h1/span[1]/a""")
+
+			err = True
 		except :
-			print("error")
+			print(link)
+			err = False
 
-		print("--" * 30 )
-		print(ingredients.text)
-		#print(category.text)
-		#print(brand_name.text)
+		if err:
+			product = [company.text,brand_name.text,category.text ,ingredients.text,link]
+			products.append(product)
+	fields = ['company', 'product name ', 'category' ,'ingredients', 'link']
+	with open('ulta.csv', 'w') as f:
 
-def target():
-	pass
+		# Create a CSV writer object that will write to the file 'f'
+		csv_writer = csv.writer(f)
 
-		#content = driver.page_source
-		#print(content)
-#driver.get("https://www.sephora.com/search?keyword=mango%20butter")
-#pop= driver.find_element(By.XPATH, """//*[@id="modal0Dialog"]/button""")
-#pop.click()
+		# Write the field names (column headers) to the first row of the CSV file
+		csv_writer.writerow(fields)
 
-#driver.get("https://www.target.com/s?searchTerm=mango+butter+")
-#search = driver.find_element(By.XPATH, """//*[@id="site_search_input"]""")
-
-#search.send_keys("mango")
+		# Write all of the rows of data to the CSV file
+		csv_writer.writerows(products)
 
 
 ulta()
+
+#target()
--- test1.py
+++ test1.py
@@ -1,40 +1,69 @@
-
-import urllib.request
-import ssl
-import requests
 from selenium import webdriver
-
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import time
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-def parse_ul(elem):
-    result = {}
-    for sub in elem.find_all('li', recursive=False):
-        if sub.a is None:
-            continue
-        data = {k: v for k, v in sub.a.attrs.items() if k != 'class'}
-        if sub.ul is not None:
-            # recurse down
-            data['children'] = parse_ul(sub.ul)
-        result[sub.a.get_text(strip=True)] = data
-    return result
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
 
 
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-
+#//div[@data-comp='Search ']//div[@data-comp='Search ']
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
-
-driver.get("https://www.ulta.com/search?search=mango+butter+")
+driver.get("https://www.sephora.com/search?keyword=hair%20oil")
+wait = WebDriverWait(driver, 10)
 driver.implicitly_wait(20)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-print(soup)
-ul      = parse_ul(soup.ul)
 
-print(len(ul))
+try:
+	outside_usa = driver.find_element(By.XPATH , """//button[@aria-label='Continue shopping']//*[name()='svg']""")
+	outside_usa.click()
+except:
+	pass
+
+while True:
+	try:
+		wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".css-bk5oor.eanm77i0"))).click()
+		time.sleep(5)
+	except:
+		break
+
+#page = driver.find_element(By.TAG_NAME , 'a')
+elems = driver.find_elements(By.XPATH ,"//a[@href]" )
+
+
+links = []
+for elem in elems:
+
+	if '/product/' in elem.get_attribute("href") :
+		links.append(elem.get_attribute("href"))
+
+
+for link in links:
+	driver.get(link)
+
+	try:
+		outside_usa = driver.find_element(By.XPATH, """//button[@aria-label='Continue shopping']//*[name()='svg']""")
+		outside_usa.click()
+	except:
+		pass
+	webdriver.support.expected_conditions.visibility_of("""//button[@data-at='ingredients']""")
+	try:
+
+		click_agian = driver.find_element(By.XPATH,"""//button[@data-at='ingredients']""")
+		click_agian.click()
+
+		ingredients = driver.find_element(By.XPATH,"""//div[@id='ingredients']""")
+		print(ingredients.text)
+		#category = driver.find_element(By.XPATH, """//ul[@id='Breadcrumbs__List']""")
+		brand_name = driver.find_element(By.XPATH, """//h1[@class='css-11zrkxf eanm77i0']""")
+		print(brand_name.text)
+		company = driver.find_element(By.XPATH, """//h1[@class='css-11zrkxf eanm77i0']""")
+
+
+		err = True
+	except:
+		print(link)
+		err = False
+
